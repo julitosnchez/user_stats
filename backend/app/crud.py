c66635a490f5368d6f3cc0c8583a671be57a6d30
juliosanchezmiron <j.sanchez@bi4group.com>
2024-06-11 18:22:04 +0200

    Connecting to DB and adding get/post user

---
+++ backend/app/crud.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
